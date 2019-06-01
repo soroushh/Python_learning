d661bfd9da6774e1a95a20db8adb8231dc30b7e0
student <sorush.kh68@gmail.com>
2019-06-01 19:36:19 +0100

    It is good to do something on strings and getting input and see its effects on your program.

---
+++ strings.py
@@ -0,0 +1,19 @@
+str = "Hello man, how are you babe?"
+
+print(str[3])
+
+print(str[3:5])
+
+print(str.split(","))
+
+print(str.lower())
+
+print(str.upper())
+
+print(len(str))
+
+print("What is your name?")
+
+name = input()
+
+print("Your name is " + name)
