3e860290031483114991ee7d0058de9041322b15
student <sorush.kh68@gmail.com>
2019-06-01 19:43:33 +0100

    We can use operators in python.

---
+++ operators.py
@@ -0,0 +1,12 @@
+x = 10
+
+y = 12
+
+if x>9 or y < 11 :
+    print("One of the conditions are correct.")
+
+if x>9 and y < 11 :
+    print("both of the conditions are correct")
+else:
+    print("Maybe one of the conditions are correct.")
+    
