1f1a35e64d86d75e8fbb9f7bae18256ef6762ab3
student <sorush.kh68@gmail.com>
2019-06-12 11:37:34 +0100

    We can make classes and define objects in them.

---
+++ cla_ob.py
@@ -0,0 +1,10 @@
+class Person :
+    def __init__ (self, name, family):
+        self.name = name
+        self.family = family
+    def full_name(self):
+        return("The full name is "+ self.name +" "+ self.family)
+
+soroush = Person("soroush","khosravi")
+
+print(soroush.full_name())
