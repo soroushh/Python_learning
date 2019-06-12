1400869a4502999aacd1fe078d6c267d7a2eed1a
student <sorush.kh68@gmail.com>
2019-06-12 11:27:26 +0100

    we can make arrays, iterate over them and use them in python.

---
+++ arr.py
@@ -0,0 +1,11 @@
+py_array = ["soroush","farnaz","rostam", "kati", "sohrab"]
+for name in py_array:
+    print(name)
+
+py_array.append("Farah")
+
+print(py_array)
+
+py_array.remove("soroush")
+
+print(py_array)
