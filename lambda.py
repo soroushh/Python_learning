5fc4c9811d9ad103589bce5856f8d0442d3d59cb
student <sorush.kh68@gmail.com>
2019-06-12 11:06:12 +0100

    lambdas can be used in methods in python.

--- lambda.py
+++ lambda.py
@@ -1,3 +1,10 @@
 lambda_function = lambda name,family : "The full name is " + name +" "+ family
 
 print(lambda_function("soroush","khosravi"))
+
+def lambda_user(number):
+    return lambda x : x*number
+
+first = lambda_user(6)
+
+print(first(5))
